dd47f34a71be96bd0047cc28051396adb82ae8d4
Kamil Koterba <kamil@wikia-inc.com>
2016-06-08 22:18:30 +0200

    Add support for error messages

--- assets/scripts/urlCutter.js
+++ assets/scripts/urlCutter.js
@@ -1,10 +1,20 @@
 (function () {
+  'use strict';
   var apiUrl = 'http://54.174.44.247/api/shorten/';
   var fieldLongUrl = 'long_url';
   var fieldShortUrl = 'short_url';
+  var inputElement = document.getElementById('urlInput');
+  var instructionElement = document.getElementById('instructions');
+  var messages = {
+    instructionsDefault: instructionElement.innerHTML,
+    instructionsErrorGeneral: 'Something went wrong. Please try again.',
+    instructionsErrorWrongUrl: 'Enter a valid URL',
+    instructionsSuccess: 'Here\'s your short URL',
+  };
 
   function init() {
     var form = document.getElementById('cutUrlForm');
+
     if (form.attachEvent) {
       form.attachEvent('submit', processForm);
     } else {
@@ -12,25 +22,56 @@
     }
   }
 
-  function processForm(e) {
-    if (e.preventDefault) {
-      e.preventDefault();
-    }
+  function resetErrorState() {
+    inputElement.className = 'cut-field';
+    instructionElement.className = 'instructions';
+    instructionElement.innerHTML = messages.instructionsDefault;
+  }
 
+  function showError(message) {
+    inputElement.className = 'cut-field--errored';
+    instructionElement.className = 'instructions--errored';
+    instructionElement.innerHTML = message;
+  }
+
+  function showSuccess() {
+    instructionElement.innerHTML = messages.instructionsSuccess;
+  }
+
+  function sendRequest() {
     var data = {};
     var xhr;
 
-    data[fieldLongUrl] = document.getElementById('urlInput').value;
+    data[fieldLongUrl] = inputElement.value;
     xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', function () {
+    xhr.addEventListener('readystatechange', function eventListener() {
       if (this.readyState === 4) {
-        document.getElementById('urlInput').value =
-          JSON.parse(this.responseText)[fieldShortUrl];
+        switch (this.status) {
+          case 400:
+            showError(messages.instructionsErrorWrongUrl);
+            break;
+          case 200:
+            showSuccess();
+            inputElement.value =
+              JSON.parse(this.responseText)[fieldShortUrl];
+            break;
+          default:
+            showError(messages.instructionsErrorGeneral);// TODO Add logging report
+        }
       }
     });
 
     xhr.open('POST', apiUrl);
     xhr.send(JSON.stringify(data));
+  }
+
+  function processForm(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    resetErrorState();
+    sendRequest();
     return false;
   }
 
